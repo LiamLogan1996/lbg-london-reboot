4d6d267d9f0a47dac944aacdb45c45792df16abe
Liam Logan <liamlogan1996@outlook.com>
2022-11-30 12:03:41 +0000

    Merge FrontEnd Changes with Spending

--- lbgLondonReboot/lbgLondonReboot/urls.py
+++ lbgLondonReboot/lbgLondonReboot/urls.py
@@ -1,5 +1,4 @@
 """lbgLondonReboot URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
@@ -16,7 +15,6 @@ Including another URLconf
 from django.conf.urls import include
 from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
 from reboot import views
 
 urlpatterns = [
--- lbgLondonReboot/reboot/views.py
+++ lbgLondonReboot/reboot/views.py
@@ -5,38 +5,70 @@ from .forms import InputForm
 
 def index(request):
     form = InputForm()
-    if request.method == 'POST':
-        form = InputForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return results(request)
-        else:
-            print(form.errors)
-    return render(request, 'reboot/index.html', {'form': form})
-
-
-def results(request):
+    context_dict = {}
     bill_spend = Product.objects.filter(
         ProductCategory='Bills').order_by('-ProductPrice')
-    context_dict = {'bill_product': bill_spend}
-    misc_spend = Product.objects.filter(
-        ProductCategory='MiscSpend').order_by('-ProductPrice')
-    context_dict['misc_product'] = misc_spend
+    context_dict['bill_product'] = bill_spend
+    food_spend = Product.objects.filter(
+        ProductCategory='Food').order_by('-ProductPrice')
+    context_dict['food_product'] = food_spend
     subscription_spend = Product.objects.filter(
         ProductCategory='Subscriptions').order_by('-ProductPrice')
     context_dict['sub_product'] = subscription_spend
-    user_input = Input.objects.last()
-    salary = (user_input.salary_input)
-    inflation = (user_input.inflation_input)
-    context_dict['salary'] = salary
-    context_dict['inflation'] = inflation
-    context_dict['bill_spend'] = round(total(bill_spend), 2)
-    context_dict['misc_spend'] = round(total(misc_spend), 2)
-    context_dict['sub_spend'] = round(total(subscription_spend), 2)
-    context_dict['bill_percent'] = percent(context_dict['bill_spend'], salary)
-    context_dict['sub_percent'] = percent(context_dict['sub_spend'], salary)
-    context_dict['misc_percent'] = percent(context_dict['misc_spend'], salary)
+    restaurant_spend = Product.objects.filter(
+        ProductCategory='Restaurants').order_by('-ProductPrice')
+    context_dict['rest_product'] = restaurant_spend
+    alcohol_spend = Product.objects.filter(
+        ProductCategory='Alcohol').order_by('-ProductPrice')
+    context_dict['alcohol_product'] = alcohol_spend
+    if request.method == 'POST':
+        form = InputForm(request.POST)
+        if form.is_valid():
+            request.session['salary_input'] = form.cleaned_data['salary_input']
+            salary_input = request.session['salary_input']
+            return results(request, salary_input)
+        else:
+            print(form.errors)
+    context_dict['form'] = form
+    return render(request, 'reboot/index.html', context_dict)
 
+
+def results(request, salary_input):
+    bill_spend = Product.objects.filter(
+        ProductCategory='Bills').order_by('-ProductPrice')
+    context_dict = {'bill_product': bill_spend}
+    food_spend = Product.objects.filter(
+        ProductCategory='Food').order_by('-ProductPrice')
+    context_dict['food_product'] = food_spend
+    subscription_spend = Product.objects.filter(
+        ProductCategory='Subscriptions').order_by('-ProductPrice')
+    context_dict['sub_product'] = subscription_spend
+    restaurant_spend = Product.objects.filter(
+        ProductCategory='Restaurants').order_by('-ProductPrice')
+    context_dict['rest_product'] = restaurant_spend
+    alcohol_spend = Product.objects.filter(
+        ProductCategory='Alcohol').order_by('-ProductPrice')
+    context_dict['alcohol_product'] = alcohol_spend
+    context_dict['salary'] = salary_input
+    salary = salary_input
+    context_dict['bill_spend'] = round(total(bill_spend), 2)
+    context_dict['food_spend'] = round(total(food_spend), 2)
+    context_dict['sub_spend'] = round(total(subscription_spend), 2)
+    context_dict['rest_spend'] = round(total(restaurant_spend), 2)
+    context_dict['alcohol_spend'] = round(total(alcohol_spend), 2)
+    context_dict['bill_percent'] = percent(context_dict['bill_spend'], salary)
+    context_dict['food_percent'] = percent(context_dict['food_spend'], salary)
+    context_dict['sub_percent'] = percent(context_dict['sub_spend'], salary)
+    context_dict['rest_percent'] = percent(context_dict['rest_spend'], salary)
+    context_dict['alcohol_percent'] = percent(context_dict['alcohol_spend'], salary)
+    spare = round(salary - (context_dict['bill_spend'] + context_dict['food_spend'] +
+                                            context_dict['sub_spend'] + context_dict['rest_spend'] +
+                                            context_dict['alcohol_spend']), 2)
+
+    if spare > 0:
+        context_dict['message'] = "You have the following left over at the end of the month £" + str(spare) +"."
+    else:
+        context_dict['message'] = "You are currently spending more than you are bring in. Please check out our help page for advice on what to do next."
     return render(request, 'reboot/results.html', context_dict)
 
 
